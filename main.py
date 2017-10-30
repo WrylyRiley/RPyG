edb1425bc7f69d93972dd37e063df2e8d74e9652
Zach Bauer <zbauer91@gmail.com>
2017-10-30 13:48:34 -0400

    Added enemy and some logic for selecting attack

--- main.py
+++ main.py
@@ -6,7 +6,31 @@ magic = [{"name": "Fire", "cost": 10, "dmg": 60},
          {"name": "Blizzard", "cost": 10, "dmg": 60}]
 
 player = Person(460, 65, 60, 34, magic)
+enemy = Person(1200, 65, 45, 25, magic)
 
-print(player.generate_spell_damage(0))
-print(player.generate_spell_damage(1))
-print(player.generate_spell_damage(2))
+running = True
+
+print(bcolors.FAIL + bcolors.BOLD + "ENEMY ATTACKS!" + bcolors.ENDC)
+while running:
+    print("===========================")
+    player.choose_action()
+    index = int(input("Choose an action")) - 1
+    print("You chose:", index)
+
+    if index == 0:
+        dmg = player.generate_damage()
+        enemy.take_damage(dmg)
+        print("You attacked for", dmg, "points of damage. Enemy HP:", enemy.get_hp())
+    elif index == 1:
+        player.choose_magic()
+        index = int(input("Choose magic")) - 1
+
+
+    enemy_choice = 0
+    enemy_dmg = enemy.generate_damage()
+    player.take_damage(enemy_dmg)
+    print("Enemy attacks for", enemy_dmg, "Player HP:", player.get_hp())
+
+
+
+    running = False
